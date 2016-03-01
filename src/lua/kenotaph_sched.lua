b08dae4a96562038d86f4be6720dfb64a56984ab
antagon <antagon@codeward.org>
2016-03-01 05:02:28 +0100

    Use proper OOP model.

--- src/lua/kenotaph_sched.lua
+++ src/lua/kenotaph_sched.lua
@@ -1,99 +1,184 @@
-function NewKenotaphAction ()
-	return {
-		cmd = nil,
-		toff = {
-			t_begin = 0,
-			t_end = 0
-		},
+KenotaphTimeRange = {
+	t_from = 0,
+	t_to = 0,
 
-		strToWeekTimeOffset = function (str)
-			--local date_str = os.date ("%w %H:%M")
-			local wday, hour, minute = string.match (str, "^(%d+) (%d+):(%d+)$")
+	strWeekTimeOffToInt = function (str)
+		--local date_str = os.date ("%w %H:%M")
+		local wday, hour, minute = string.match (str, "^(%d+) (%d+):(%d+)$")
 
-			return (wday * 86400 + hour * 3600 + minute * 60)
-		end,
+		if wday == nil or hour == nil or minute == nil then
+			return nil
+		end
 
-		setTimeOffset = function (self, which, time_str)
-			local time_off = self.strToWeekTimeOffset (time_str)
+		return (wday * 86400 + hour * 3600 + minute * 60)
+	end,
 
-			if time_off == nil then
-				return false
+	isInRange = function (self, time)
+		print ("Time: " .. time .. " From: " .. self.t_from .. " To: " .. self.t_to)
+		return (time >= self.t_from and time < self.t_to)
+	end
+}
+
+KenotaphAction = {
+	name = action_name,
+	cmd = nil,
+	wtoff = {},
+
+	addTimeRange = function (self, strtoff_beg, strtoff_end)
+		print ("Adding a time range (" .. strtoff_beg .. ", " .. strtoff_end .. ") for '" .. self.name .. "'")
+		local time_range = KenotaphTimeRange:new (strtoff_beg, strtoff_end)
+
+		table.insert (self.wtoff, time_range)
+
+		return time_range
+	end,
+
+	getTimeRanges = function (self)
+		return self.wtoff
+	end
+}
+
+KenotaphDevice = {
+	id = device_id,
+	action = {
+		a_beg = {},
+		a_end = {},
+		a_err = {}
+	},
+
+	addAction = function (self, atype, name)
+		print ("Adding an action '" .. name .. "' (" .. atype ..  ")")
+
+		atype = string.lower (atype)
+
+		-- Check whether atype is in defined range (i.e. beg/end/err)
+		if self.action["a_" .. atype] == nil then
+			return nil
+		end
+
+		self.action["a_" .. atype][name] = KenotaphAction:new (name)
+		return self.action["a_" .. atype][name]
+	end,
+
+	getAction = function (self, atype, name)
+		atype = string.lower (atype)
+
+		-- Check whether atype is in defined range (i.e. beg/end/err)
+		if self.action["a_" .. atype] == nil then
+			return nil
+		end
+
+		return self.action["a_" .. atype][name]
+	end,
+
+	getImmediateAction = function (self, atype)
+		atype = string.lower (atype)
+
+		-- Check whether atype is in defined range (i.e. beg/end/err)
+		if self.action["a_" .. atype] == nil then
+			return nil
+		end
+
+		local weekoff_now = KenotaphTimeRange.strWeekTimeOffToInt (os.date ("%w %H:%M"))
+
+		-- TODO: optimize algorithm by sorting the array by begin time offset
+		for action_idx, action in pairs (self.action["a_" .. atype]) do
+			for timerange_idx, time_range in ipairs (action:getTimeRanges ()) do
+				if time_range:isInRange (weekoff_now) then
+					return action
+				end
 			end
-
-			self.toff[which] = time_off
-
-			return true
-		end,
-
-		setBeginTimeOffset = function (self, time_str)
-			return self.setTimeOffset (self, "t_begin", time_str)
-		end,
-
-		setEndTimeOffset = function (self, time_str)
-			return self.setTimeOffset (self, "t_end", time_str)
-		end,
-
-		setCmd = function (self, cmd)
-			self.cmd = cmd
-		end
-	}
-end
-
-function NewKenotaphDevice ()
-	return {
-		action = {
-			a_begin = nil,
-			a_end = nil,
-			a_error = nil
-		},
-
-		addAction = function (self, atype, name)
-			print ("Adding an action " .. name .. "(" .. atype ..  ")")
-			self.action[atype] = NewKenotaphAction ()
-			return self.action[atype]
 		end
 
-		-- TODO...
-		-- sortActions
-	}
+		return nil
+	end
+}
+
+KenotaphServer = {
+	name = server_name,
+	device = {},
+
+	addDevice = function (self, id)
+		print ("Adding a device '" .. id .. "'")
+		self.device[id] = KenotaphDevice:new (id)
+		return self.device[id]
+	end,
+
+	getDevice = function (self, name)
+		return self.device[name]
+	end
+}
+
+KenotaphSched = {
+	server = {},
+
+	addServer = function (self, name)
+		print ("Adding a server '" .. name .. "'")
+		self.server[name] = KenotaphServer:new (name)
+		return self.server[name]
+	end,
+
+	getServer = function (self, name)
+		return self.server[name]
+	end
+}
+
+function KenotaphTimeRange:new (strtoff_beg, strtoff_end)
+	local time_range = {}
+
+	setmetatable (time_range, KenotaphTimeRange)
+
+	self.__index = self
+
+	time_range.t_from = self.strWeekTimeOffToInt (strtoff_beg)
+	time_range.t_to = self.strWeekTimeOffToInt (strtoff_end)
+
+	return time_range
 end
 
-function NewKenotaphServer (server_name)
-	return {
-		name = server_name,
-		device = {},
+function KenotaphAction:new (name)
+	local action = {}
 
-		addDevice = function (self, name)
-			print ("Adding a device " .. name)
-			self.device[name] = NewKenotaphDevice ()
-			return self.device[name]
-		end,
+	setmetatable (action, KenotaphAction)
 
-		getDevice = function (self, name)
-			return self.device[name]
-		end
-	}
+	self.__index = self
+
+	action.name = name
+
+	return action
 end
 
-function NewKenotaphSched ()
-	return {
-		server = {},
+function KenotaphDevice:new (id)
+	local device = {}
 
-		addServer = function (self, name)
-			print ("Adding a server " .. name)
-			self.server[name] = NewKenotaphServer (name)
-			return self.server[name]
-		end,
+	setmetatable (device, KenotaphDevice)
 
-		getServer = function (self, name)
-			return self.server[name]
-		end
-	}
+	self.__index = self
+
+	device.id = id
+
+	return device
 end
 
---[[local Kenotaph = NewKenotaphSched ()
+function KenotaphServer:new (name)
+	local server = {}
 
-local keno_server = Kenotaph:addServer ("my_testserver")
+	setmetatable (server, KenotaphServer)
 
-local keno_dev = keno_server:addDevice ("my_testdevice")--]]
+	self.__index = self
+
+	server.name = name
+
+	return server
+end
+
+function KenotaphSched:new ()
+	local resource = {}
+
+	setmetatable (resource, KenotaphSched)
+
+	self.__index = self
+
+	return resource
+end
 
--- src/lua/main.lua
+++ src/lua/main.lua
@@ -1,5 +1,20 @@
 require "kenotaph_sched"
 
+local KenotaphSched = KenotaphSched:new ()
+
+KenotaphSched:addServer ("test_server1")
+
+KenotaphSched:getServer ("test_server1"):addDevice ("bb_phone")
+
+KenotaphSched:getServer ("test_server1"):getDevice ("bb_phone"):addAction ("beg", "test_action1_beg")
+KenotaphSched:getServer ("test_server1"):getDevice ("bb_phone"):addAction ("end", "test_action1_end")
+
+KenotaphSched:getServer ("test_server1"):getDevice ("bb_phone"):getAction ("beg", "test_action1_beg"):addTimeRange ("2 02:20", "2 03:29")
+KenotaphSched:getServer ("test_server1"):getDevice ("bb_phone"):getAction ("end", "test_action1_end"):addTimeRange ("2 02:20", "2 03:29")
+
+--
+-- Test input data...
+--
 local argv = {
 	server = "test_server1",
 	msg = "BEG bb_phone wlan0:1\n",
@@ -12,10 +27,6 @@ local argv = {
 	}
 }
 
-local Kenotaph = NewKenotaphSched ()
-
-Kenotaph:addServer ("test_server1")
-
 function main (argv)
 	if argv == nil then
 		return 1
@@ -23,19 +34,32 @@ function main (argv)
 
 	local arg = argv
 
-::_continue::
 	while arg do
-		local lserver = Kenotaph:getServer (arg.server)
+		local server = KenotaphSched:getServer (arg.server)
 
-		if lserver == nil then
-			goto _continue
+		if server ~= nil then
+			io.write ("== GOT message " .. arg.msg)
+
+			local evt_type, id, iface = string.match (arg.msg, "^([%a%p]+) ([%w%p]+) ([%w%p]+)\n$")
+
+			local device = server:getDevice (id)
+
+			if device ~= nil then
+				io.write ("Device " .. device.id .. "\n")
+
+				local action = device:getImmediateAction (evt_type)
+
+				if action ~= nil then
+					io.write ("Now running by the action " .. action.name .. "\n")
+				else
+					io.write ("No scheduled action for this event\n")
+				end
+			else
+				io.write ("Unknown device " .. id .. "\n")
+			end
+		else
+			io.write ("server " .. lserver .. " not found...\n")
 		end
-
-		print ("Server " .. lserver.name .. " found...")
-
-		local evt_type, id, iface = string.match (arg.msg, "^([%a%p]+) ([%w%p]+) ([%w%p]+)\n$")
-
-		print (evt_type, id, iface)
 
 		arg = arg.next
 	end
